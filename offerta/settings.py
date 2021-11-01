cfc11d9ea71651718c8be7656b0ed218e31443dd
Marcos Pereira da Silva Cruz <marcospcruz@gmail.com>
2021-11-01 18:08:13 -0300

    Atualização do ALLOWED_HOSTS

--- offerta/settings.py
+++ offerta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'put your secret key here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['offertatest.herokuapp.com','127.0.0.1']
 
 
 # Application definition
